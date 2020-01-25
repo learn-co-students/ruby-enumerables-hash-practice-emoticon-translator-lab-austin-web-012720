e673c04ab96056049e22501e6e378c92f1a821f3
mystical-calculator-5374 <bbrower@smu.edu>
2020-01-25 06:07:41 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,40 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
+def load_library(file)
   # code goes here
+  emoticons_file = YAML.load_file(file)
+
+  emoticons_hash = {
+    get_emoticon: {},
+    get_meaning: {}
+  }
+
+  emoticons_file.each do |key, array|
+    array.each do |emoticon|
+      emoticons_hash[:get_emoticon][array.first] = array.last
+      emoticons_hash[:get_meaning][array.last] = key
+    end
+  end
+  emoticons_hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file, emoticon)
+  emoticons = load_library(file)
+  if !emoticons[:get_emoticon].has_key?(emoticon)
+    return "Sorry, that emoticon was not found"
+  else
+    return emoticons[:get_emoticon][emoticon]
+  end
 end
 
-def get_english_meaning
+def get_english_meaning(file, emoticon)
   # code goes here
+  emoticons = load_library(file)
+  if !emoticons[:get_meaning].has_key?(emoticon)
+    return "Sorry, that emoticon was not found"
+  else
+    return emoticons[:get_meaning][emoticon]
+  end
 end
